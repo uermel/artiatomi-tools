29de3f89e556d2b84b13db7291884396a2498cfc
Utz Ermel <utzermel@gmail.com>
2019-11-05 17:36:05 +0100

    bugfixes and improved input/comments

--- refineAlign/example.m
+++ refineAlign/example.m
@@ -1,4 +1,10 @@
 opts = struct();
+
+% General options
+opts.projDir = '/project/directory/'; % Any non-existing directory
+opts.iters = 10;
+
+% Executable locations and remote setup
 opts.cAligner = '/path/to/cAligner';
 opts.EmSART = 'EmSART';
 opts.EmSARTRefine = 'EmSARTRefine';
@@ -7,25 +13,47 @@ opts.STA_dir = 'path/to/SubTomogramAverageMPI';
 opts.remote = true;
 opts.host = 'localhost';
 
-
-opts.projDir = '/project/directory/';
-opts.iters = 10;
-opts.motl = artia.em.read('/path/to/start/motl.em');
+% Reconstruction parameters
 opts.projFile = '/path/to/tiltstack.st';
-opts.tomoNr = 1;
 opts.markerFile = '/path/to/markerfile.em';
+opts.reconDim = [928 928 350]; %typical 1k size
+opts.imDim = [927 959]; % 1k image stack
+opts.volumeShifts = [0 0 -100];
+opts.maAmount = 1;
+opts.maAngle = 0;
+opts.voxelSize = 1;
+
+% Averaging parameters
+opts.tomoNr = 78;
+opts.boxSize = 64;
+opts.motl = artia.em.read('/path/to/start/motl.em');
 opts.wedge = artia.em.read('/path/to/wedge.em');
 opts.mask = artia.em.read('/path/to/mask.em');
 opts.reference = artia.em.read('/path/to/reference.em'); 
 opts.maskCC = artia.em.read('/path/to/maskCC.em');
-opts.reconDim = [928 928 200]; %typical 1k size
-opts.imDim = [3708 3838]; % 4k image stack
-opts.volumeShifts = [0 0 0];
+opts.angIter = 3;
+opts.angIncr = 5;
+opts.phiAngIter = 3;
+opts.phiAngIncr = 5;
+opts.avgLowPass = 12;
+opts.avgHighPass = 0;
+opts.avgSigma = 3;
+
+% Refinement (projection matching) parameters
+opts.groupMode = 'MaxDistance';
+opts.maxDistance = 150;
 opts.groupSize = 20;
-opts.maxShift = 30;
-opts.speedUpDist = 100;
-opts.voxelSize = 4;
-opts.boxSize = 64;
+opts.maxShift = 15;
+opts.speedUpDist = 60;
 
+% Refinement band pass filter
+opts.lowPass = 200;
+opts.lowPassSigma = 50;
+opts.highPass = 20;
+opts.highPassSigma = 10;
 
+% Volume size computation
+opts.borderSize = 5*opts.boxSize;
+
+% RUN!
 iterative_alignment(opts)
--- refineAlign/iterative_alignment.m
+++ refineAlign/iterative_alignment.m
@@ -5,7 +5,7 @@ function iterative_alignment(opts)
     mkdir(opts.projDir);
     
     for i = 1:opts.iters
-        % Make directories and names and save files %%%%%%%%%%%%%%%%%%%%%
+        % Make directories and names and save files %%%%%%%%%%%%%%%%%%%%%%%
         disp('------------------------------------------')
         disp(['Iter ' num2str(i)])
         disp('Preparing ...')
@@ -57,7 +57,7 @@ function iterative_alignment(opts)
         refCfgFile = sprintf([iterDir 'tomo_%d_refine_iter_%d.cfg'], opts.tomoNr, i);
         avgCfgFile = sprintf([iterDir 'avg_iter_%d.cfg'], i);
         
-        % Make configs %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Make configs %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         disp('Writing cfgs ...')
         oss10 = recon_blankConfig();
         oss1 = recon_blankConfig();
@@ -74,6 +74,7 @@ function iterative_alignment(opts)
         oss10.PhiAngle = num2str(phi);
         oss10.VoxelSize = num2str(opts.voxelSize);
         oss10.SIRTCount = '10';
+        oss10.MagAnisotropy = [num2str(opts.maAmount) ' ' num2str(opts.maAngle)];
         artia.cfg.write(oss10, oss10CfgFile);
         
         %oss1
@@ -85,6 +86,7 @@ function iterative_alignment(opts)
         oss1.PhiAngle = num2str(phi);
         oss1.VoxelSize = num2str(opts.voxelSize);
         oss1.SIRTCount = '1';
+        oss1.MagAnisotropy = [num2str(opts.maAmount) ' ' num2str(opts.maAngle)];
         artia.cfg.write(oss1, oss1CfgFile);
         
         %oss1_filtered
@@ -97,10 +99,11 @@ function iterative_alignment(opts)
         oss1_filtered.VoxelSize = num2str(opts.voxelSize);
         oss1_filtered.SIRTCount = '1';
         oss1_filtered.SkipFilter = 'false';
-        oss1_filtered.fourFilterLP = '200';
-        oss1_filtered.fourFilterLPS = '50';
-        oss1_filtered.fourFilterHP = '10';
-        oss1_filtered.fourFilterHPS = '5';
+        oss1_filtered.fourFilterLP = num2str(opts.lowPass);
+        oss1_filtered.fourFilterLPS = num2str(opts.lowPassSigma);
+        oss1_filtered.fourFilterHP = num2str(opts.highPass);
+        oss1_filtered.fourFilterHPS = num2str(opts.highPassSigma);
+        oss1_filtered.MagAnisotropy = [num2str(opts.maAmount) ' ' num2str(opts.maAngle)];
         artia.cfg.write(oss1_filtered, oss1FilteredCfgFile);
         
         %refine
@@ -114,12 +117,15 @@ function iterative_alignment(opts)
         refine.VoxelSize = num2str(opts.voxelSize);
         refine.SIRTCount = '1';
         refine.SkipFilter = 'false';
-        refine.fourFilterLP = '200';
-        refine.fourFilterLPS = '50';
-        refine.fourFilterHP = '10';
-        refine.fourFilterHPS = '5';
+        refine.fourFilterLP = num2str(opts.lowPass);
+        refine.fourFilterLPS = num2str(opts.lowPassSigma);
+        refine.fourFilterHP = num2str(opts.highPass);
+        refine.fourFilterHPS = num2str(opts.highPassSigma);
+        refine.MagAnisotropy = [num2str(opts.maAmount) ' ' num2str(opts.maAngle)];
+        refine.GroupMode = opts.groupMode;
         refine.GroupSize = num2str(opts.groupSize);
         refine.MaxShift = num2str(opts.maxShift);
+        refine.MaxDistance = num2str(opts.maxDistance);
         refine.Reference = refFile;
         refine.SizeSubVol = num2str(opts.boxSize);
         refine.SpeedUpDistance = num2str(opts.speedUpDist);
@@ -127,7 +133,7 @@ function iterative_alignment(opts)
         refine.CCMapFileName = ccmapPre;
         refine.MotiveList = refMotlFile;
         refine.ShiftOutputFile = shiftFile;
-        refine.MultiPeakDetection = false;
+        refine.MultiPeakDetection = 'false';
         artia.cfg.write(refine, refCfgFile);
         
         %avg
@@ -140,99 +146,50 @@ function iterative_alignment(opts)
         avg.NamingConvention = 'TomoParticle';
         avg.StartIteration = '1';
         avg.EndIteration = '2';
-        avg.AngIter = '3';
-        avg.AngIncr = '5';
-        avg.PhiAngIter = '3';
-        avg.PhiAngIncr = '5';
-        avg.LowPass = '10';
-        avg.HighPass = '1';
-        avg.Sigma = '2';
+        avg.AngIter = num2str(opts.angIter);
+        avg.AngIncr = num2str(opts.angIncr);
+        avg.PhiAngIter = num2str(opts.phiAngIter);
+        avg.PhiAngIncr = num2str(opts.phiAngIncr);
+        avg.LowPass = num2str(opts.avgLowPass);
+        avg.HighPass = num2str(opts.avgHighPass);
+        avg.Sigma = num2str(opts.avgSigma);
         artia.cfg.write(avg, avgCfgFile);
         
-        % Run the reconstructions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Run the reconstructions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         disp('Running reconstructions ...')
-        % Write the run script
-        %runScript = [iterDir 'run_script.sh'];
-        %fileID = fopen(runScript,'w');
-        %fprintf(fileID,'%s\n','cd /home/Group/Software/tomography/kunzFunctions/EmSART/');
-        %com = 'mpiexec -n 4 EmSART -u ';
-        %fprintf(fileID,'%s%s\n %s%s\n %s%s\n', com, oss10CfgFile, ...
-                                               %com, oss1CfgFile, ...
-                                               %com, oss1FilteredCfgFile);                                        
-        %fclose(fileID);
-        
-        % Execute
-        %[~, ~] = system(['chmod +x '  runScript]);
-        %system(['ssh -t borg ' runScript]);
         artia.mpi.run(opts.EmSART, 4, oss10CfgFile, 'runRemote', opts.remote, 'remoteHost', opts.host, 'suppressOutput', false)
         artia.mpi.run(opts.EmSART, 4, oss1CfgFile, 'runRemote', opts.remote, 'remoteHost', opts.host, 'suppressOutput', false)
         artia.mpi.run(opts.EmSART, 4, oss1FilteredCfgFile, 'runRemote', opts.remote, 'remoteHost', opts.host, 'suppressOutput', false)
         
-        % Get Parts %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Get Parts %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         tomolist = {};
         tomolist{opts.tomoNr} = oss1ReconFile;
-        artia.particle.extract_write(extractMotlFile, 1, tomolist, opts.boxSize/2, 0, 0, 1, partFilePre);
-        %writeParts(extractMotlFile, parts, );
-        
+        artia.particle.extract_write(extractMotlFile, 1, tomolist, opts.boxSize/2, 0, 0, 1, partFilePre);        
         
         % Run the averaging %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         disp('Running averaging ...')
-        % Write the run script
-        %runScript = [iterDir 'run_script.sh'];
-        %fileID = fopen(runScript,'w');
-        %fprintf(fileID,'%s\n','cd /home/kunz/DevelopSicherung/SubTomogramAverageMPI/SubTomogramAverageMPI/SubTomogramAverageMPI');
-        %fprintf(fileID,'%s%s\n','mpiexec -n 4 SubTomogramAverageMPI -u ', avgCfgFile);
-        %fclose(fileID);
-
-        % Run the command
-        %[~, ~] = system(['chmod +x '  runScript]);
-        %system(['ssh -t borg ' runScript]);
         artia.mpi.run(opts.STA, 4, avgCfgFile, 'runRemote', opts.remote, 'remoteHost', opts.host, 'suppressOutput', false, 'execDir', opts.STA_dir)
         
-        % Do the refinement %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Do the refinement %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         disp('Doing the refinement ...')
-        % Write the run script
-        %runScript = [iterDir 'run_script.sh'];
-        %fileID = fopen(runScript,'w');
-        %fprintf(fileID,'%s\n','cd /home/Group/Software/tomography/kunzFunctions/EmSART/');
-        %com = 'EmSARTRefine -u ';
-        %fprintf(fileID,'%s%s\n', com, refCfgFile);
-        %fclose(fileID);
-        
-        % Execute
-        %[~, ~] = system(['chmod +x '  runScript]);
-        %system(['ssh -t borg ' runScript]);
         artia.mpi.run(opts.EmSARTRefine, 1, refCfgFile, 'runRemote', opts.remote, 'remoteHost', opts.host, 'suppressOutput', false);
         
-        % Project the particles with the shift %%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Project the particles with the shift %%%%%%%%%%%%%%%%%%%%%%%%%%%%
         disp('Backprojecting particles with shift ...')
+        % Get shifts
         shifts = artia.em.read(shiftFile);
+        % Get aligned particles
         motive = artia.em.read(refMotlFile);
         motive(8:10, :) = motive(8:10, :) + motive(11:13, :);
         motive(11:13, :) = 0;
-        %[marker, model, phi] = emreadCoords(currMarkFile);
+        
+        % Get marker
         mFile = artia.marker.read(currMarkFile);
         marker = mFile.ali;
         phi = mFile.beamDeclination;
-        %projCount = size(marker, 2);
-
-        %exclude = zeros(projCount, 1);
-        %for j = 1:projCount
-        %    if marker(2, j, 1) < 0 || marker(3, j, 1) < 0
-        %        exclude(j) = 1;
-        %    end
-        %end
         
-        %newMarker = ConvertAmira3DCoordinatesToClicker2DCoordinatesShift(motive, marker, opts.reconDim, [7420 7676], opts.voxelSize, 0, shifts);
-        [newMarker, model] = artia.geo.project3D(motive(8:10, :), marker, phi, opts.reconDim, opts.imDim, opts.voxelSize, opts.volumeShifts, 1.016, 42, shifts);
+        [newMarker, model] = artia.geo.project3D(motive(8:10, :), marker, phi, opts.reconDim, opts.imDim, opts.voxelSize, opts.volumeShifts, opts.maAmount, opts.maAngle, shifts);
         
-        %for j = 1:projCount
-        %    if exclude(j) == 1
-        %        newMarker(2, j, :) = -1;
-        %        newMarker(3, j, :) = -1;
-        %        newMarker(4:end, j, :) = 0;
-        %    end
-        %end
         mFile.ali = newMarker;
         mFile.model = model;
         mFile.markerCount = size(motive, 2);
@@ -240,64 +197,54 @@ function iterative_alignment(opts)
         newMarkFile = sprintf([iterDir 'tomo_%d_new_marker_unaligned_iter_%d.em'], opts.tomoNr, i);
         alignedMarkFile = sprintf([iterDir 'tomo_%d_new_marker_aligned_iter_%d.em'], opts.tomoNr, i);
         artia.marker.write(mFile, newMarkFile);
-        %emwrite_silent(newMarker, newMarkFile);
         
-        % Run the alignment %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Run the alignment %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        disp('Running the 3D alignment ...')
+        % Figure out the central marker
         positions = opts.motl(8:10, :) + opts.motl(11:13, :);
         centroid = mean(positions, 2);
         dist = sqrt((positions(1, :) - centroid(1)).^2 + (positions(2, :) - centroid(2)).^2 + (positions(3, :) - centroid(3)).^2);
-        %diff = sum((positions - centroid).^2');
         [~, idx] = min(dist);
         
-        disp('Running the 3D alignment ...')
-        % Write the run script
-        %runScript = [iterDir 'run_script.sh'];
-        %fileID = fopen(runScript,'w');
-        format = '%s -i %s -o %s -a %d -w %d -h %d --maAmount 1.016 --maAngle 42 --phi %f --iter 20 --iterSwitch 10 --doPsi --doTheta\n';
-        com = sprintf(format, opts.cAligner, newMarkFile, alignedMarkFile, idx-1, opts.imDim(1), opts.imDim(2), mFile.beamDeclination);
-        %fclose(fileID);
+        % Run first alignment for beam declination
+        format1 = '%s -i %s -o %s -a %d -w %d -h %d --maAmount %f --maAngle %f --phi %f --iter 20 --iterSwitch 10 --doPsi --doFixedPsi --doPhi\n';
+        com1 = sprintf(format1, opts.cAligner, newMarkFile, alignedMarkFile, idx-1, opts.imDim(1), opts.imDim(2), opts.maAmount, opts.maAngle, mFile.beamDeclination);
+        system(com1);
         
-        % Execute
-        %[~, ~] = system(['chmod +x '  runScript]);
-        system(com);
+        % Load file to get beam declination
+        mFile = artia.marker.read(alignedMarkFile);
         
-        % Retrieve the new coordinates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Run second alignment for tilt angles + image roation
+        format2 = '%s -i %s -o %s -a %d -w %d -h %d --maAmount %f --maAngle %f --phi %f --iter 20 --iterSwitch 10 --doPsi --doTheta\n';
+        com2 = sprintf(format2, opts.cAligner, newMarkFile, alignedMarkFile, idx-1, opts.imDim(1), opts.imDim(2), opts.maAmount, opts.maAngle, mFile.beamDeclination);
+        system(com2);
+        
+        % Retrieve the new coordinates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         disp('Making new motivelist ...')
-        %[alignedMarker, coords, phi] = emreadCoords(alignedMarkFile);
         mark = artia.marker.read(alignedMarkFile);
         alignedMarker = mark.ali;
         coords = mark.model;
         phi = mark.beamDeclination;
     
-        % Bring in correct orientation, binning and add half the recon dim
+        % Bring in correct orientation
         mx = -coords(2:3:end);
         my = coords(1:3:end);
         mz = coords(3:3:end);
         
-        opts.volumeShifts = [-mean([max(mx), min(mx)]) -mean([max(my), min(my)]) -mean([max(mz), min(mz)])];
+        % Compute new volume shifts to center the particles in the volume
+        opts.volumeShifts = round([-mean([max(mx), min(mx)]) -mean([max(my), min(my)]) -mean([max(mz), min(mz)])]);
         
-        %x = mx./opts.voxelSize + 0.5 + opts.reconDim(1)/2;
-        %y = my./opts.voxelSize + 0.5 + opts.reconDim(2)/2;
-        %z = mz./opts.voxelSize;
-        
-        %newopts.reconDim = [opts.reconDim(1), opts.reconDim(2), round(std(z)) + 50];
-        %z = z + newopts.reconDim(3)/2 + 0.5;
-        
-
-        %x = -coords(2:3:end)./opts.voxelSize + opts.reconDim(1)/2 + 0.5*opts.voxelSize;
-        %y = coords(1:3:end)./opts.voxelSize + opts.reconDim(2)/2 + 0.5*opts.voxelSize;
-        %z = coords(3:3:end)./opts.voxelSize;
-        %newopts.reconDim = [opts.reconDim(1), opts.reconDim(2), round(std(z)) + 50];
-        %z = z + newopts.reconDim(3)/2 + 0.5*opts.voxelSize;
-        
+        % Correct binning
         x = (mx + opts.volumeShifts(1))./opts.voxelSize;
         y = (my + opts.volumeShifts(2))./opts.voxelSize;
         z = (mz + opts.volumeShifts(3))./opts.voxelSize;
         
-        xr = 4*floor((round(max(x) + abs(min(x))) + 2*opts.boxSize)/4);
-        yr = 4*floor((round(max(y) + abs(min(y))) + 2*opts.boxSize)/4);
-        zr = 4*floor((round(max(z) + abs(min(z))) + 2*opts.boxSize)/4);
+        % Volume size computation (max/min coord + opts.borderSize)
+        xr = 4*floor((round(max(x) + abs(min(x))) + opts.borderSize)/4);
+        yr = 4*floor((round(max(y) + abs(min(y))) + opts.borderSize)/4);
+        zr = 4*floor((round(max(z) + abs(min(z))) + opts.borderSize)/4);
         
+        % Volume size divisible by 2
         if mod(xr, 2) ~= 0
             xr = xr+1;
         end
@@ -308,12 +255,16 @@ function iterative_alignment(opts)
             zr = zr+1;
         end
         
+        % New volume dimensions
         newreconDim = [xr, yr, zr];
         
+        % Now compute the final particle coords in the new volume
+        % dimensions
         x = x + newreconDim(1)/2 + 0.5;
         y = y + newreconDim(2)/2 + 0.5;
         z = z + newreconDim(3)/2 + 0.5;
 
+        % Assign everything to the particle list and save
         new_motl = zeros(20, size(alignedMarker, 3));
         new_motl(8, :) = round(x);
         new_motl(9, :) = round(y);
@@ -329,9 +280,7 @@ function iterative_alignment(opts)
         opts.motl = new_motl;
         opts.markerFile = alignedMarkFile;
         opts.reconDim = newreconDim;
-        %opts.volumeShifts = [0 0 0];
         
         disp('Done ...')
     end
-
 end
